2e5e78c0069e01d9ab54598670b61ab039486a14
home <126045264+CCTTBG@users.noreply.github.com>
2025-12-17 17:51:03 +0900

    보스 선택 창에 작성한 보스 카드 출력

--- src/component/bossSelect.vue
+++ src/component/bossSelect.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
       <p>보스 선택</p>
+      <bossCard></bossCard>
   </div>
 </template>
 <script>
+  import bossCard from './bossCard.vue';
 export default {
+  components:{
+    bossCard
+  },
+  data(){
 
+  }
 }
 </script>
 <style>
