bbdf2f9635c5ad0faa7648bbcf4c234d17f42f3c
home <126045264+CCTTBG@users.noreply.github.com>
2025-12-29 20:26:44 +0900

    신화카드 셋팅을 위한 기능 개발 및 컴포넌트 작성

--- src/component/mainGamePage.vue
+++ src/component/mainGamePage.vue
@@ -1,14 +1,45 @@
 <template>
   <div>
-    <h1>보스 상세</h1>
-    <p>bossId: {{ bossId }}</p>
+    <h1>메인 게임판</h1>
+    <p>신화 단계 진행 횟수: {{ mythosTurns }}</p>
+
+    <mythosCardDeck
+      :bossId="bossId"
+      :difficulty="difficulty"
+      @draw="onDraw"
+    />
+
   </div>
 </template>
 
 <script>
+import mythosCardDeck from './mythosCardDeck.vue'
+
 export default {
-  props: {
-    bossId: { type: String, required: true },
+  components: { mythosCardDeck },
+  props: { bossId: { type: String, required: true } },
+
+  data() {
+    return {
+      mythosTurns: 0,      // 신화 단계 진행 횟수
+      drawnTotal: 0        // 총 뽑은 카드 수(원하면 사용)
+    }
   },
+
+  computed: {
+    difficulty() {
+      // 예시 규칙: 1~3턴 easy, 4~6턴 normal, 7턴부터 hard
+      if (this.mythosTurns < 3) return 'easy'
+      if (this.mythosTurns < 6) return 'normal'
+      return 'hard'
+    }
+  },
+
+  methods: {
+    onDraw(drawnCards) {
+      this.mythosTurns += 1
+      this.drawnTotal += drawnCards.length
+    }
+  }
 }
 </script>
